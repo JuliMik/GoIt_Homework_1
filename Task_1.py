bb23c7ccfde286c7a20a5fcfed590f2c74f8e0d2
JuliMik <gusachenkojulia:gmail.com>
2024-10-14 15:25:14 +0100

    Create task

--- Task_1.py
+++ Task_1.py
@@ -1,1 +1,17 @@
-print(2)
+from _datetime import datetime
+
+
+def get_days_from_today(given_date):
+    get_day = datetime.strptime(given_date, "%Y-%m-%d").date()
+    current_day = datetime.today().date()
+    difference_days = current_day - get_day
+    return difference_days.days
+
+
+while True:
+    try:
+        given_date = input('Please, enter the date in format (yyyy-mm-dd): ')
+        print(get_days_from_today(given_date))
+        break
+    except ValueError:
+        print('Invalid date!')
