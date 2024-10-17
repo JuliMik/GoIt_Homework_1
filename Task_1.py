8fd4178fc4226cacfe9a0a3702f8857d2854aa85
JuliMik <gusachenkojulia:gmail.com>
2024-10-17 13:37:59 +0100

    Improved function

--- Task_1.py
+++ Task_1.py
@@ -2,16 +2,14 @@ from _datetime import datetime
 
 
 def get_days_from_today(given_date):
-    get_day = datetime.strptime(given_date, "%Y-%m-%d").date()
-    current_day = datetime.today().date()
-    difference_days = current_day - get_day
-    return difference_days.days
-
-
-while True:
     try:
-        given_date = input('Please, enter the date in format (yyyy-mm-dd): ')
-        print(get_days_from_today(given_date))
-        break
+        get_day = datetime.strptime(given_date, "%Y-%m-%d").date()
+        current_day = datetime.today().date()
+        difference_days = current_day - get_day
+        return difference_days.days
     except ValueError:
         print('Invalid date!')
+
+
+given_date = input('Please, enter the date in format (yyyy-mm-dd): ')
+print(get_days_from_today(given_date))
